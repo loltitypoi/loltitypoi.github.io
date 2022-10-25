76c24a3389b68ae9cb30a9e0f7572c9929828a50
loltitypoi <116634808+loltitypoi@users.noreply.github.com>
2022-10-25 12:43:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let phrases = [
   { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
   { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' },
   { text: 'просидеть всеь день в тик токе', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' },
-  { text: 'сходить в кфс', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+  { text: 'сходить в кфс', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' 
 ];
 
 function getRandomElement(arr) {
